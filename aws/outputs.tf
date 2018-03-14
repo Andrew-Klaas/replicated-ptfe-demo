bac2bcb2d6f96492b0b30a80251b746d8defbc77
Dan Brown <roooms@gmail.com>
2018-03-14 16:31:01 +0000

    Split install types into modules

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -2,6 +2,10 @@ output "replicated console password" {
   value = "${random_pet.replicated-pwd.id}"
 }
 
+output "ptfe fqdn(s)" {
+  value = "${list(module.demo.demo_fqdn, module.pes.pes_fqdn, module.pmd.pmd_fqdn)}"
+}
+
 output "next steps" {
-  value = "To finish the installation visit https://${aws_route53_record.demo.fqdn}:8800 to install your license file, choose the release channel and installation type."
+  value = "To finish the installation visit https://<fqdn>:8800 to install your license file, choose the release channel and installation type."
 }
---
+++ aws/pes/variables.tf
@@ -0,0 +1,12 @@
+variable "namespace" {}
+variable "aws_instance_ami" {}
+variable "aws_instance_type" {}
+variable "ssh_key_name" {}
+variable "owner" {}
+variable "ttl" {}
+variable "user_data" {}
+variable "subnet_id" {}
+variable "vpc_security_group_ids" {}
+variable "hashidemos_zone_id" {}
+variable "database_pwd" {}
+variable "db_subnet_group_name" {}
